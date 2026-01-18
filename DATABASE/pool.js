fc332d0ec78cf4b38d2068d91f39184959747d9a
Loremispum <141552551+loremispum2k8@users.noreply.github.com>
2026-01-18 22:21:36 +0200

    ahhhh

--- DATABASE/pool.js
+++ DATABASE/pool.js
@@ -1,6 +1,5 @@
 const { Pool } = require("pg");
-require('dotenv').config()
 module.exports = new Pool({
     connectionString: "postgresql://inventoryappdatabase_6rfj_user:i9dtxs5T7Zgms4jnEQ5uA6ZNDW2Oitir@dpg-d5l2rdpr0fns738tul30-a/inventoryappdatabase_6rfj",
-    ssl:false
+    ssl:{rejectUnauthorized:false}
 })
